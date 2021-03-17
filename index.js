3d3089e7000eb3f722905360ad7f3542d56114d2
Eli Trop <eli.trop@checkmarx.com>
2021-03-17 18:34:56 +0200

    My first action is ready

---
+++ index.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+import { sarifToMarkdown } from "@security-alert/sarif-to-markdown";
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const sarifReportpath = core.getInput('sarifReport');
+  console.log(`report file name ${sarifReportpath}!`);
+  let rawdata = fs.readFileSync(sarifReportpath);
+  const results = sarifToMarkdown({
+    owner: github.owner,
+    repo: github.repo,
+    branch: github.branch,
+    sourceRoot: ""
+})(JSON.parse(sarifContent));
+  const time = (new Date()).toTimeString();
+  core.setOutput("mdFile", results);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
