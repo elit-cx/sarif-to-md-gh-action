6c9b32512562ed39538135baad063765ba373ea4
Eli Trop <eli.trop@checkmarx.com>
2021-03-17 18:47:37 +0200

    fix content

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ const sariftToMd = require('@security-alert/sarif-to-markdown')
 try {
   // `who-to-greet` input defined in action metadata file
   const sarifReportpath = core.getInput('sarifReport');
-  console.log(`report file name ${sarifReportpath}!`);
+  console.log(`report file name ${sarifReportpath}`);
   let rawdata = fs.readFileSync(sarifReportpath);
+  console.log(`get sarif data ${rawdata}`);
   const results = sariftToMd.sarifToMarkdown({
     owner: github.owner,
     repo: github.repo,
     branch: github.branch,
     sourceRoot: ""
 })(JSON.parse(rawdata));
+console.log(`convert sarif data ${results}`);
   const time = (new Date()).toTimeString();
   core.setOutput("mdFile", results);
   // Get the JSON webhook payload for the event that triggered the workflow
